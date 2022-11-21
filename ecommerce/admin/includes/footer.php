a322ada609eb9d17c2f6d2f3a40e4419443de4d5
HashJProgramming <joshuamarceloambalong@gmail.com>
2022-11-22 07:05:26 +0800

    first commit

---
+++ ecommerce/admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2022 TLS - TeamLearningSession <a href="#">Hash'J Programming</a></strong>
+</footer>
